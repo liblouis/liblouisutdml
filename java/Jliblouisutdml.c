895d9b5910ee3dd6acb2a062a8fa80b0cc0728e6
John Boyer <john.boyer@abilitiessoft.com>
2011-01-27 10:59:31 -0600

    various fixes

--- java/Jliblouisutdml.c
+++ java/Jliblouisutdml.c
@@ -30,23 +30,23 @@
 #include <louisutdml.h>
 
 /*
- * Class:     org_liblouis_Jliblouisutdml
+ * Class:     org_liblouis_liblouisutdml
  * Method:    version
  * Signature: ()Ljava/lang/String;
  */
-JNIEXPORT jstring JNICALL Java_org_liblouis_Jliblouisutdml_version
+JNIEXPORT jstring JNICALL Java_org_liblouis_liblouisutdml_version
   (JNIEnv * env, jobject this)
 {
   return (*env)->NewStringUTF (env, lbu_version ());
 }
 
 /*
- * Class:     org_liblouis_Jliblouisutdml
+ * Class:     org_liblouis_liblouisutdml
  * Method:    translateString
  * Signature: (Ljava/lang/String;[B[B[ILjava/lang/String;Ljava/lang/String;I)Z
  */
 JNIEXPORT jboolean JNICALL
-Java_org_liblouis_Jliblouisutdml_translateString (JNIEnv * env,
+Java_org_liblouis_liblouisutdml_translateString (JNIEnv * env,
 						  jobject this,
 						  jstring
 						  configFileList,
@@ -105,12 +105,12 @@ release:
 }
 
 /*
- * Class:     org_liblouis_Jliblouisutdml
+ * Class:     org_liblouis_liblouisutdml
  * Method:    backTranslateString
  * Signature: (Ljava/lang/String;[B[B[ILjava/lang/String;Ljava/lang/String;I)Z
  */
 JNIEXPORT jboolean JNICALL
-Java_org_liblouis_Jliblouisutdml_backTranslateString (JNIEnv * env,
+Java_org_liblouis_liblouisutdml_backTranslateString (JNIEnv * env,
 						      jobject this,
 						      jstring
 						      configFileList,
@@ -172,11 +172,11 @@ release:
 }
 
 /*
- * Class:     org_liblouis_Jliblouisutdml
+ * Class:     org_liblouis_liblouisutdml
  * Method:    translateFile
  * Signature: (Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;I)Z
  */
-JNIEXPORT jboolean JNICALL Java_org_liblouis_Jliblouisutdml_translateFile
+JNIEXPORT jboolean JNICALL Java_org_liblouis_liblouisutdml_translateFile
   (JNIEnv * env, jobject this, jstring configFileList, jstring inputFileName,
    jstring outputFileName, jstring logFile, jstring settingsString, jint mode)
 {
@@ -217,12 +217,12 @@ release:
 }
 
 /*
- * Class:     org_liblouis_Jliblouisutdml
+ * Class:     org_liblouis_liblouisutdml
  * Method:    translateTextFile
  * Signature: (Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;I)Z
  */
 JNIEXPORT jboolean JNICALL
-Java_org_liblouis_Jliblouisutdml_translateTextFile (JNIEnv * env,
+Java_org_liblouis_liblouisutdml_translateTextFile (JNIEnv * env,
 						    jobject this,
 						    jstring
 						    configFileList,
@@ -271,12 +271,12 @@ release:
 }
 
 /*
- * Class:     org_liblouis_Jliblouisutdml
+ * Class:     org_liblouis_liblouisutdml
  * Method:    backTranslateFile
  * Signature: (Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;I)Z
  */
 JNIEXPORT jboolean JNICALL
-Java_org_liblouis_Jliblouisutdml_backTranslateFile (JNIEnv * env,
+Java_org_liblouis_liblouisutdml_backTranslateFile (JNIEnv * env,
 						    jobject this,
 						    jstring
 						    configFileList,
@@ -325,11 +325,11 @@ release:
 }
 
 /*
- * Class:     org_liblouis_Jliblouisutdml
+ * Class:     org_liblouis_liblouisutdml
  * Method:    charToDots
  * Signature: (Ljava/lang/String;[B[BLjava/lang/String;I)V
  */
-JNIEXPORT jboolean JNICALL Java_org_liblouis_Jliblouisutdml_charToDots
+JNIEXPORT jboolean JNICALL Java_org_liblouis_liblouisutdml_charToDots
   (JNIEnv * env, jobject this, jstring tableList, jbyteArray inbuf,
    jbyteArray outbuf, jstring logFile, jint mode)
 {
@@ -366,11 +366,11 @@ release:
 }
 
 /*
- * Class:     org_liblouis_Jliblouisutdml
+ * Class:     org_liblouis_liblouisutdml
  * Method:    dotsToChar
  * Signature: (Ljava/lang/String;[B[BLjava/lang/String;I)V
  */
-JNIEXPORT jboolean JNICALL Java_org_liblouis_Jliblouisutdml_dotsToChar
+JNIEXPORT jboolean JNICALL Java_org_liblouis_liblouisutdml_dotsToChar
   (JNIEnv * env, jobject this, jstring tableList, jbyteArray inbuf,
    jbyteArray outbuf, jstring logFile, jint mode)
 {
@@ -407,11 +407,11 @@ release:
 }
 
 /*
- * Class:     org_liblouis_Jliblouisutdml
+ * Class:     org_liblouis_liblouisutdml
  * Method:    checkTable
  * Signature: (Ljava/lang/String;Ljava/lang/String;I)V
  */
-JNIEXPORT jboolean JNICALL Java_org_liblouis_Jliblouisutdml_checkTable
+JNIEXPORT jboolean JNICALL Java_org_liblouis_liblouisutdml_checkTable
   (JNIEnv * env, jobject this, jstring tableList, jstring logFile, jint mode)
 {
   const jbyte *tableListX = NULL;
@@ -433,22 +433,22 @@ release:
 }
 
 /*
- * Class:     org_liblouis_Jliblouisutdml
+ * Class:     org_liblouis_liblouisutdml
  * Method:    charSize
  * Signature: ()I
  */
-JNIEXPORT jint JNICALL Java_org_liblouis_Jliblouisutdml_charSize
+JNIEXPORT jint JNICALL Java_org_liblouis_liblouisutdml_charSize
   (JNIEnv * env, jobject this)
 {
   return CHARSIZE;
 }
 
 /*
- * Class:     org_liblouis_Jliblouisutdml
+ * Class:     org_liblouis_liblouisutdml
  * Method:    free
  * Signature: ()V
  */
-JNIEXPORT void JNICALL Java_org_liblouis_Jliblouisutdml_free
+JNIEXPORT void JNICALL Java_org_liblouis_liblouisutdml_free
   (JNIEnv * env, jobject this)
 {
   lbu_free ();
@@ -456,7 +456,7 @@ JNIEXPORT void JNICALL Java_org_liblouis_Jliblouisutdml_free
 }
 
 /*
- * Class:     org_liblouis_Jliblouisutdml
+ * Class:     org_liblouis_liblouisutdml
  * Method:    file2brl
  * Signature: ([Ljava/lang/String;)Z
  */
@@ -481,7 +481,7 @@ getArg (JNIEnv * env, jobject this, jobjectArray args, jint index)
   return curArg;
 }
 
-JNIEXPORT jboolean JNICALL Java_org_liblouis_Jliblouisutdml_file2brl
+JNIEXPORT jboolean JNICALL Java_org_liblouis_liblouisutdml_file2brl
   (JNIEnv * env, jobject this, jobjectArray args)
 {
   jint numArgs = (*env)->GetArrayLength (env, args);
@@ -522,7 +522,7 @@ JNIEXPORT jboolean JNICALL Java_org_liblouis_Jliblouisutdml_file2brl
 	      break;
 	    case 't':
 	      mode |= htmlDoc;
-k++;
+	      k++;
 	      break;
 	    case 'f':
 	      strcpy (configFileList, getArg (env, this, args, k + 1));
@@ -533,7 +533,7 @@ k++;
 	    case 'r':
 	    case 'x':
 	      whichProc = curArg[1];
-k++;
+	      k++;
 	      break;
 	    case 'C':
 	      if (configSettings == NULL)
@@ -545,10 +545,10 @@ k++;
 	      k += 2;
 	      strcat (configSettings, "\n");
 	      break;
-case '-':
-	/* Input file is stdin but output file is not stdout.*/
-k++;
- break;
+	    case '-':
+	      /* Input file is stdin but output file is not stdout. */
+	      k++;
+	      break;
 	    default:
 	      lou_logPrint ("invalid argument%s", curArg);
 	      return JNI_FALSE;
@@ -559,13 +559,15 @@ k++;
 	{
 	  if (k == numArgs - 1)
 	    {
-	      strcpy (inputFileName, curArg);
+	      strcpy (outputFileName, curArg);
+	      k++;
 	    }
 	  else if (k == numArgs - 2)
 	    {
-		strcpy (inputFileName, curArg);
+	      strcpy (inputFileName, curArg);
 	      strcpy (outputFileName, getArg (env, this, args, k + 1));
-k += 2;	    }
+	      k += 2;
+	    }
 	  else
 	    {
 	      lou_logPrint ("extra operand: %s\n",
