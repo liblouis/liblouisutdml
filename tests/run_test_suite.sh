2db27785ab41f6cef46461bc7a6595f83051b3b5
Bert Frees <bertfrees@gmail.com>
2013-09-09 13:32:17 +0200

    change license on run_test_suite

--- tests/run_test_suite.sh
+++ tests/run_test_suite.sh
@@ -4,9 +4,20 @@
 #
 # Copyright (C) 2013 by Swiss Library for the Blind, Visually Impaired and Print Disabled 
 #
-# Copying and distribution of this file, with or without modification,
-# are permitted in any medium without royalty provided the copyright
-# notice and this notice are preserved.
+# This file is free software; you can redistribute it and/or modify it
+# under the terms of the Lesser or Library GNU General Public License as
+# published by the Free Software Foundation; either version 3, or (at
+# your option) any later version.
+
+# This file is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the Library
+# GNU General Public License for more details.
+
+# You should have received a copy of the Library GNU General Public
+# License along with this program; see the file COPYING.  If not, write
+# to the Free Software Foundation, 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301, USA.
 
 verbose=1
 colors=1
