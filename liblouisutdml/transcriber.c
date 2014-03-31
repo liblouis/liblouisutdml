8258632903303a52ac46f91aed14df6d17ef32e0
Michael Whapples <mwhapples@gmail.com>
2014-03-31 17:34:45 +0100

    Fixed for obtaining the correct buffer size after edit table, fixes illegal character errors in BrailleBlaster.

--- liblouisutdml/transcriber.c
+++ liblouisutdml/transcriber.c
@@ -5332,7 +5332,7 @@ utd_editTrans ()
       translatedLength = MAX_TRANS_LENGTH;
       if (!lou_translate
 	  (ud->edit_table_name, ud->text_buffer, &translationLength,
-	   ud->translated_buffer, &translatedLength, NULL, NULL, NULL,
+	   ud->translated_buffer, &ud->translated_length, NULL, NULL, NULL,
 	   NULL, NULL, dotsIO))
 	{
 	  lou_logPrint
