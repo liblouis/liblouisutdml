fdc2da75746695a12d07752560bd30920d8a50d8
Michael Whapples <Michael Whapples@localhost>
2014-07-10 11:48:49 +0100

    Beginning of a test framework, currently hard coded test but should be possible to externalise to data file

---
+++ java/tests/org/liblouis/SimpleTest.java
@@ -0,0 +1,71 @@
+package org.liblouis;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.util.List;
+import java.util.ArrayList;
+
+import org.testng.annotations.*;
+import org.testng.Assert;
+
+import org.liblouis.LibLouisUTDML;
+
+public class SimpleTest {
+  int testInt;
+  LibLouisUTDML lbu;
+  @BeforeClass
+  public void LoadLibLouisUTDML() throws Exception {
+    LibLouisUTDML.loadLibrary(System.getProperty("liblouis.dir"), System.getProperty("liblouis.ext"));
+    lbu = LibLouisUTDML.getInstance();
+  }
+  @BeforeMethod
+  public void setup() {
+    testInt = 0;
+  }
+  @DataProvider(name="translateFileTest")
+  public Object[][] translateFileData() {
+    List<Object> testCaseData = new ArrayList();
+    testCaseData.add("nimas.cfg");
+    testCaseData.add(new File("testdata", "list-test.xml").getAbsolutePath());
+    testCaseData.add(new File("testdata", "output.utd").getAbsolutePath());
+    testCaseData.add(null);
+    testCaseData.add(null);
+    testCaseData.add(0);
+    return new Object[][] {testCaseData.toArray()};
+  }
+  @Test
+  public void addTest() {
+    testInt++;
+    assert(testInt == 1);
+  }
+  @Test
+  public void subtractTest() {
+    testInt--;
+    assert(testInt == -1);
+  }
+  @Test(dataProvider="translateFileTest")
+  public void testTranslateFile(String configList, String inFile, String expectedFile, String logFile, String settings, int mode) throws Exception {
+    // Perform the translation of the file
+    if (!lbu.translateFile(configList, inFile, new File("testdata", "actual.utd").getAbsolutePath(), logFile, settings, mode)) {
+      throw new Exception("LibLouisUTDML could not perform translation of the file " + inFile);
+    }
+    // Now compare the results
+    BufferedReader actualReader = new BufferedReader(new FileReader(new File("testdata", "actual.utd")));
+    BufferedReader expectedReader = new BufferedReader(new FileReader(new File(expectedFile)));
+    String actualLine;
+    String expectedLine;
+    // Do not use the && shortcut as we always want to read both files
+    while (((actualLine = actualReader.readLine()) != null) & ((expectedLine = expectedReader.readLine()) != null)) {
+      if (!actualLine.equals(expectedLine)) {
+        // Set one of the line strings to null so that fail will be detected
+        actualLine = null;
+        break;
+      }
+    }
+    // Check for failure, only one of actualLine or expectedLine will be null
+    if ((actualLine == null) ^ (expectedLine == null)) {
+      Assert.fail("The actual file does not match the expected file");
+    }
+  }
+}
