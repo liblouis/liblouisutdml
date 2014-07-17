81df7952dcbed9d2f017e8d2078b682f7f714bef
Michael Whapples <Michael Whapples@localhost>
2014-07-17 07:55:29 +0100

    Make log levels final in Java bindings.

--- java/src/org/liblouis/LogLevel.java
+++ java/src/org/liblouis/LogLevel.java
@@ -2,11 +2,11 @@ package org.liblouis;
 
 public final class LogLevel
 {
-  public static int ALL = -2147483648;
-  public static int DEBUG = 10000;
-  public static int INFO = 20000;
-  public static int WARNING = 30000;
-  public static int ERROR = 40000;
-  public static int FATAL = 50000;
-  public static int OFF = 2147483647;
+  public final static int ALL = -2147483648;
+  public final static int DEBUG = 10000;
+  public final static int INFO = 20000;
+  public final static int WARNING = 30000;
+  public final static int ERROR = 40000;
+  public final static int FATAL = 50000;
+  public final static int OFF = 2147483647;
 }
