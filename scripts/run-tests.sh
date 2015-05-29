0da09534bf079a90f536fc59a1c22f8328f8bb36
Christian Egli <christian.egli@sbs.ch>
2015-05-29 16:36:59 +0200

    make sure bleeding edge liblouis is downloaded properly

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -ev
 if [ "${LIBLOUIS_VERSION}" = "master" ]; then
-    wget https://github.com/liblouis/liblouis/archive/master.tar.gz
+    LIBLOUIS_DOWNLOAD_URL=https://github.com/liblouis/liblouis/archive/master.tar.gz
 else
-    wget https://github.com/liblouis/liblouis/releases/download/v${LIBLOUIS_VERSION}/liblouis-${LIBLOUIS_VERSION}.tar.gz
+    LIBLOUIS_DOWNLOAD_URL=https://github.com/liblouis/liblouis/releases/download/v${LIBLOUIS_VERSION}/liblouis-${LIBLOUIS_VERSION}.tar.gz
 fi
-tar -xf liblouis-*.tar.gz
+wget -O liblouis-release.tar.gz ${LIBLOUIS_DOWNLOAD_URL}
+tar -xf liblouis-release.tar.gz
 cd liblouis-* && ./configure && make && sudo make install && cd ..
 sudo ldconfig
 ./autogen.sh && ./configure && make && make check
