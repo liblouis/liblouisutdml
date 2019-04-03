4577460692c957d103b8b43ff1fc0673c243e754
Christian Egli <christian.egli@sbs.ch>
2019-04-03 12:52:26 +0200

    Convert the test suite to a standard Automake Parallel Test Harness

---
+++ tests/run_test.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+# tests/run_test_suite.sh.  Generated from run_test_suite.sh.in by configure.
+
+# run_test_suite
+#
+# Copyright (C) 2013,2016 by Swiss Library for the Blind, Visually Impaired and Print Disabled 
+#
+# This file is free software; you can redistribute it and/or modify it
+# under the terms of the Lesser or Library GNU General Public License as
+# published by the Free Software Foundation; either version 3, or (at
+# your option) any later version.
+
+# This file is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the Library
+# GNU General Public License for more details.
+
+# You should have received a copy of the Library GNU General Public
+# License along with this program; see the file COPYING.  If not, write
+# to the Free Software Foundation, 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301, USA.
+
+# Create a temporary directory $tmp_dir in $TMPDIR (default /tmp).
+: ${TMPDIR=/tmp}
+{
+    tmp_dir=`(umask 077 && mktemp -d "$TMPDIR/fooXXXXXX") 2>/dev/null` && test -d "$tmp_dir"
+} || exit $?
+
+find-up () {
+    current=$(pwd)
+    path=$current
+    while [[ "$path" != "" && ! -e "$path/$1" ]]; do
+	path=${path%/*}
+    done
+    if [ "$path" != "" ]; then
+	echo $(realpath --relative-to=$current $path/$1)
+    else
+	exit 1;
+    fi
+}
+
+run_test () {
+    test_dir=$1
+    cd "$test_dir"
+    README=$(find-up README)
+    input=$(find-up input.xml)
+    styles=$(find-up styles.cfg)
+    expected=$(find-up expected.txt)
+    file2brl -w $tmp_dir -f $styles $input $tmp_dir/output.txt 2> /dev/null
+    if [ $? -ne 0 ]; then
+	exit 99;
+    else
+	diff -q $expected $tmp_dir/output.txt >/dev/null
+	if [ $? -ne 0 ]; then
+	    if [ -n "$VERBOSE" ]; then
+		cat $README
+		echo "Diff: " >&2
+		diff -u $expected $tmp_dir/output.txt
+	    fi
+	    exit 1;
+	else
+	    echo "bah"
+	    rm $tmp_dir/output.txt
+	    exit 0;
+	fi
+    fi
+    rm -f file2brl.temp
+    rm -rf $tmp
+}
+
+run_test $1
